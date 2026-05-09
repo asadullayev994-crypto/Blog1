e4c61336bfd3fcc699df49d3c3e4a93dce1bc5c5
akmalk2 <sadullayevakmal21@gmail.com>
2026-05-09 18:33:04 +0500

    auth

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,7 @@
-from django.urls import path,include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
-
-
-
-router = DefaultRouter()
-router.register(r'posts', PostViewSet)
+from django.urls import path
+from .views import PostListAPIView, PostDetailAPIView
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('posts/', PostListAPIView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailAPIView.as_view(), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,61 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permission import IsAuthorOrReadOnly 
 
+class PostListAPIView(APIView):
+ 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            # Post yaratilayotganda muallifni avtomatik tokendan oladi
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PostDetailAPIView(APIView):
+  
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            obj = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, obj)
+            return obj
+        except Post.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        if post is None:
+            return Response({"error": "Post topilmadi"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        if post is None:
+            return Response({"error": "Post topilmadi"}, status=status.HTTP_404_NOT_FOUND)
+            
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        if post is None:
+            return Response({"error": "Post topilmadi"}, status=status.HTTP_404_NOT_FOUND)
+            
+        post.delete()
+        return Response({"message": "Post o'chirildi"}, status=status.HTTP_204_NO_CONTENT)
